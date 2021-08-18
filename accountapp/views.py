759789522be31d12d300a3ac7f8e4a53b44857b5
YJH <success899@naver.com>
2021-08-18 18:01:41 +0900

    210818 class commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,11 +7,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 from django.urls import reverse, reverse_lazy
+
+from articleapp.models import Article
+
 
 @login_required         #login_required(login_url=reverse_lazy('accountapp:login')) 안될 땐 수동으로 url 경로 표기
 def hello_world(request):
@@ -40,10 +44,16 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list, **kwargs)
 
 
 has_ownership = [login_required, account_ownership_required]
